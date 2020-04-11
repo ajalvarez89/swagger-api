7435a1ed3156cd6207f2de9e275042c65f996413
Alvaro Alvarez <alvaro.alvarez@2transfair.com>
2020-04-11 12:43:01 -0500

    adding swagge ui

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  mount Rswag::Ui::Engine => '/api-docs'
+  mount Rswag::Api::Engine => '/api-docs'
+
+  get '/', to: redirect('/api-docs')
+
+  namespace :api do
+    namespace :v1 do
+      resources :pets
+    end
+  end
 end
